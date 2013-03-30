89e105700672ecb9bd6d310e3b8430a9dcdeeece
zahavi.mail <zahavi.mail@gmail.com>
2013-03-30 20:25:20 +0300

    login + signup screen & activity

--- src/com/calm/android/activity/CreateProjectActivityStepTwo.java
+++ src/com/calm/android/activity/CreateProjectActivityStepTwo.java
@@ -1,6 +1,9 @@
 package com.calm.android.activity;
 
+import android.os.Bundle;
+import android.widget.Spinner;
 import com.calm.android.R;
+import roboguice.inject.InjectView;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,5 +17,29 @@ public class CreateProjectActivityStepTwo extends CalmActivity {
     @Override
     protected int getLayoutId() {
         return R.layout.new_project_screen_2;
+    }
+
+    @InjectView(R.id.newproject_spinner_date_day)
+    private Spinner mDaySpinner;
+
+    @InjectView(R.id.newproject_spinner_date_month)
+    private Spinner mMonthSpinner;
+
+    @InjectView(R.id.newproject_spinner_date_year)
+    private Spinner mYearSpinner;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addListenerOnSpinnerItemSelection();
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        mDaySpinner = (Spinner) findViewById(R.id.newproject_spinner_date_day);
+        mDaySpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        mMonthSpinner = (Spinner) findViewById(R.id.newproject_spinner_date_month);
+        mMonthSpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        mYearSpinner = (Spinner) findViewById(R.id.newproject_spinner_date_year);
+        mYearSpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
     }
 }
