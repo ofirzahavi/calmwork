20c81220948239f3cf1518a3777ac008a12a663c
zahavi.mail <zahavi.mail@gmail.com>
2013-04-04 01:51:58 +0300

    login + signup screen & activity

---
+++ src/com/calm/android/activity/SkillsPopup.java
@@ -0,0 +1,15 @@
+package com.calm.android.activity;
+
+import android.app.Dialog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZAHAVI
+ * Date: 04/04/13
+ * Time: 01:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class SkillsPopup {
+
+
+}
--- src/com/calm/android/activity/TeacherHomeActivity.java
+++ src/com/calm/android/activity/TeacherHomeActivity.java
@@ -1,6 +1,7 @@
 package com.calm.android.activity;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import roboguice.inject.InjectView;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.app.Dialog;
 
 import java.util.Calendar;
 
@@ -39,6 +41,17 @@ public class TeacherHomeActivity extends CalmActivity implements CompoundButton.
     @InjectView(R.id.teacherhome_button_past_projects)
     private Button mPastProjectsButton;
 
+    @InjectView(R.id.teacherthome_button_add_skills)
+    private Button mEditSkills;
+
+   // @InjectView(R.id.newproject_spinner_level_skills)
+   // private Spinner mLevelSpinnerSkills;
+
+    //@InjectView(R.id.newproject_spinner_subject_skills)
+    //private Spinner mSubjectSpinnerSkills;
+
+    final Context context = this;
+
     @Override
     protected int getLayoutId() {
         return R.layout.teacher_home_screen;
@@ -46,6 +59,7 @@ public class TeacherHomeActivity extends CalmActivity implements CompoundButton.
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addListenerOnSpinnerItemSelection();
 
         mSwitchStudentNinja = (Switch) findViewById(R.id.teacherhome_switch_student_or_ninja);
 
@@ -54,6 +68,19 @@ public class TeacherHomeActivity extends CalmActivity implements CompoundButton.
             mSwitchStudentNinja.setOnCheckedChangeListener(this);
         }
 
+        mEditSkills.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // custom dialog
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.edit_skills_dialog);
+                dialog.setTitle("Add Skills:");
+
+                dialog.show();
+            }
+        });
     }
 
     @Override
@@ -66,6 +93,14 @@ public class TeacherHomeActivity extends CalmActivity implements CompoundButton.
             Intent intent = new Intent(getApplicationContext(), StudentHomeActivity.class);
             startActivity(intent);
         }
+
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+      // mLevelSpinnerSkills = (Spinner) findViewById(R.id.newproject_spinner_level_skills);
+      //  mLevelSpinnerSkills.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+       // mSubjectSpinnerSkills = (Spinner) findViewById(R.id.newproject_spinner_level_skills);
+        //mSubjectSpinnerSkills.setOnItemSelectedListener(new CustomOnItemSelectedListener());
 
     }
 }
